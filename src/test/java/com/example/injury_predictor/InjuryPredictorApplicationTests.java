bd37d64df369ce8d2506a2dcbbe86d7704342532
levimarkmuigai <levimarkmuigai@gmail.com>
2025-06-09 10:33:37 +0300

    Initial commit

---
+++ src/test/java/com/example/injury_predictor/InjuryPredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.injury_predictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjuryPredictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
