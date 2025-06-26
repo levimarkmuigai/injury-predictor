ec4a2e8cf06836479975aad0f3568540ccac5031
levimarkmuigai <levimarkmuigai@gmail.com>
2025-06-26 10:29:58 +0300

    Modified the model using annotations, refactored for easier readability.

--- src/main/java/com/example/InjuryPredictor/controller/AthleteProfileController.java
+++ src/main/java/com/example/InjuryPredictor/controller/AthleteProfileController.java
@@ -4,10 +4,30 @@ import com.example.InjuryPredictor.model.AthleteProfile;
 
 import com.example.InjuryPredictor.repositories.athleteRepository;
 
+import com.example.InjuryPredictor.model.PredictionRecord;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.PutMapping;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import java.util.List;
 
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api/profiles")
 public class AthleteProfileController{
     private final athleteRepository operation;
 
@@ -16,23 +36,27 @@ public class AthleteProfileController{
     }
 
     // Create AtheleteProfile
+    @PutMapping
     public AthleteProfile createAtheleteProfile(AthleteProfile athlete){
         return operation.save(athlete);
     }
 
     // List All the AthleteProfiles
+    @GetMapping
     public List<AthleteProfile> ListAllAtheleteProfiles(){
         return operation.findAll();
     }
 
     // Find AthleteProfile By id
+    @GetMapping("/{id}")
     public Optional<AthleteProfile> getAtheleteProfileById(Long id){
         return operation.findById(id);
     }
 
     // Update the AthleteProfile by id
-    public Optional<AthleteProfile> updateAthleteProfile(Long id, 
-            AthleteProfile updateProfile){
+    @PutMapping("/{id}")
+    public Optional<AthleteProfile> updateAthleteProfile(@PathVariable Long id, 
+            @RequestBody AthleteProfile updateProfile){
 
         return operation.findById(id).map(profile -> {
             profile.setFirstName(updateProfile.getFirstName());
@@ -50,6 +74,7 @@ public class AthleteProfileController{
         });
     }
 
+    @DeleteMapping("/{id}")
     public void deleteAthleteProfile(Long id){
         if(operation.existsById(id)){
             operation.deleteById(id);
