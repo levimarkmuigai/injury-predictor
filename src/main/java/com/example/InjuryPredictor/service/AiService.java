6b9e08365a6be26d845b16e948d911fe4048c657
levimarkmuigai <levimarkmuigai@gmail.com>
2025-06-28 07:53:22 +0300

    Injected BOM to the POM.

--- src/main/java/com/example/InjuryPredictor/service/AiService.java
+++ src/main/java/com/example/InjuryPredictor/service/AiService.java
@@ -2,5 +2,7 @@ package com.example.InjuryPredictor.service;
 
 import org.springframework.stereotype.Service;
 
+//import org.springframework.ai.chat.client.ChatClient;
+
 @Service
 public class AiService{}
