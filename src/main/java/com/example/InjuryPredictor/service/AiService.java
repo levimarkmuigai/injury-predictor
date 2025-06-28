6c9dd8c7b3155395006563bf1db208f997628c61
levimarkmuigai <levimarkmuigai@gmail.com>
2025-06-28 09:01:25 +0300

    Added Wrapper to enable use of ChatCleint throughout the Service Layer.

--- src/main/java/com/example/InjuryPredictor/service/AiService.java
+++ src/main/java/com/example/InjuryPredictor/service/AiService.java
@@ -13,4 +13,12 @@ public class AiService{
 
         chatClient = builder.build();
     }
+
+    public String chat(String prompt){
+        return chatClient
+            .prompt(prompt)
+            .call()
+            .content();
+    }
+
 }
