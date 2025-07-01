d33ff6bced28f72e27394072d8d174d7e7bab400
levimarkmuigai <levimarkmuigai@gmail.com>
2025-07-01 09:24:27 +0300

    Refactored for easier usage and testing.

--- src/test/java/com/example/InjuryPredictor/service/SuggestionServiceTest.java
+++ src/test/java/com/example/InjuryPredictor/service/SuggestionServiceTest.java
@@ -1,1 +1,16 @@
 package com.example.InjuryPredictor.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.InjuryPredictor.model.AthleteProfile;
+
+import com.example.InjuryPredictor.model.PredictionRecord;
+
+import java.time.LocalDate;
+
+
+class SuggestionServiceTest{}
